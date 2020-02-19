28c78510e8ff8bf2a9d192cafeee60e3552758d1
aaronmatson1 <matsonaaron1@gmail.com>
2020-02-18 21:03:37 -0700

    added Data and Forms

---
+++ team-builder/src/data.js
@@ -0,0 +1,21 @@
+export default[
+
+    {
+        name: "Aaron",
+        role: "Web Developer",
+        email: "email@email.com"
+    },
+
+    {
+        name: "Michael",
+        role: "World's Best Boss",
+        email: "Michael@DunderMifflin.com"
+
+    },
+
+    {
+        name: "Dwight Schrute",
+        role: "World's best Paper Salesman",
+        email: "Dwight@DunderMifflin.com"
+    }
+]
